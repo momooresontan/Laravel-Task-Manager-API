4ea906f1fab47539c42a4da284b31b7ab124d773
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-29 11:25:50 +0100

    task collection & resource added

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TaskCollection;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(){
-        return response()->json(Task::all());
+        return new TaskCollection(Task::all());
     }
 }
