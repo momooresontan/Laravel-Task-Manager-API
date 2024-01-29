ccf0fc476e9f7c1e048bb4f571983306f77f80b3
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-29 22:22:02 +0100

    auth controller created

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(! Auth::attempt($validated
+
+    }
+}
