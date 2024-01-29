73a5a891c92571fed6a17eced66df0d1a8bd31a2
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-29 13:41:38 +0100

    delete method added

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
@@ -23,5 +24,19 @@ class TaskController extends Controller
 
         $task = Task::create($validated);
         return new TaskResource($task);
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task){
+        $validated = $request->validated();
+
+        $task->update($validated);
+
+        return new TaskResource($task);
+    }
+
+    public function destroy(Task $task){
+        $task->delete();
+
+        return response()->noContent();
     }
 }
