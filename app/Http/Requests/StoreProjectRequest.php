b5dd8b3257fb57dd5add3fd1893cdc70c7fd4682
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-02-01 12:50:55 +0100

    store and update request modified

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -22,7 +22,7 @@ class StoreProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|max:255'
+            'title' => 'required|max:255',
         ];
     }
 }
