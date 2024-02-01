e39f58b6c5dae86179e26924669ea3b00e5440b2
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-02-01 09:28:16 +0100

    project migration creation

--- database/migrations/2024_02_01_082325_add_project_id_to_tasks.php
+++ database/migrations/2024_02_01_082325_add_project_id_to_tasks.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('projects', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->foreignId('user_id')->constrained('users');
-            $table->timestamps();
+        Schema::table('tasks', function (Blueprint $table) {
+            //
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
     }
 };
